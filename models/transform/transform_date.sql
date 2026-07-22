c817e0f34680c4cdd3b0610130f42ba70e579b5a
anushikaagarwal <69230410+anushikaagarwal@users.noreply.github.com>
2026-07-22 12:50:54 +0100

    Delete models/transform directory

--- models/transform/transform_date.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    date_key                                                            AS date_day,
-    fiscal_year_name,
-    fiscal_year_quarter_name,
-    month_number_in_fiscal_quarter,
-    fiscal_year_name || '-' || fiscal_year_quarter_name                 AS fiscal_quarter,
-    fiscal_year_name || '-' || fiscal_year_quarter_name
-        || '-M' || CAST(month_number_in_fiscal_quarter AS INT)          AS fiscal_quarter_month
-FROM {{ source('edh_shared', 'finance_calendar') }}
